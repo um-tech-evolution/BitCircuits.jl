c66853de559c276c4f0de2317fba24fa89a77c14
George Lesica <george@lesica.com>
2014-11-23 16:01:07 -0600

    Update tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using BitCircuits
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+or(left, right) = left | right
+and(left, right) = left & right
+
+# Demo using the following equivalency:
+# p <- ab + ac
+# q <- a (b + c)
+# p = q
+
+a = Variable(0)
+b = Variable(1)
+c = Variable(2)
+
+ab = Operation(and, a, b)
+ac = Operation(and, a, c)
+p = Operation(or, ab, ac)
+boc = Operation(or, b, c)
+q = Operation(and, a, boc)
+
+@test equal(p, q)
+@test evaluate(ab, 0b000011) == true
+@test evaluate(ab, 0b000010) == false
+@test evaluate(ab, 0b000001) == false
+@test evaluate(ab, 0b000000) == false
